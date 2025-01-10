228e5c105ff90734e3ddc8dbdf171f3383fff076
Rob Marvin <fenalfan@gmail.com>
2025-01-10 21:49:04 +0300

    fix: add a check for the zero length of the array to the getBalanceIndex function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -292,6 +292,7 @@ function isContainNumber(num, digit) {
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
 function getBalanceIndex(arr) {
+  if (arr.length === 0) return -1;
   let leftSum = 0;
   let rightSum = 0;
   for (let i = 0; i < arr.length - 1; i += 1) {
