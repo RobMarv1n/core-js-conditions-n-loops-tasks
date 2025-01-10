2a3a5ec8ced3f3debc2818c748602f92254fcee5
Rob Marvin <fenalfan@gmail.com>
2025-01-10 08:52:40 +0300

    feat: complete 9 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -226,8 +226,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reverStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reverStr += str[i];
+  }
+  return str === reverStr;
 }
 
 /**
@@ -244,8 +248,11 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) return i;
+  }
+  return -1;
 }
 
 /**
@@ -263,8 +270,12 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const strNum = String(num);
+  for (let i = 0; i < strNum.length; i += 1) {
+    if (+strNum[i] === digit) return true;
+  }
+  return false;
 }
 
 /**
